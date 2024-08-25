c7a3b875dd7a59de1c639cecdf09be1476164f01
Aboud <gh.abd.com.207@gmail.com>
2024-08-25 07:15:33 +0300

    create chatbot interface

--- main.py
+++ main.py
@@ -18,20 +18,6 @@ os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 
 
 from langchain_community.document_loaders import DirectoryLoader
-def load_data_from_folders(main_folder_path):
-    data = {}
-    for category_folder in os.listdir(main_folder_path):
-        category_label = category_folder 
-        category_path = os.path.join(main_folder_path, category_folder)
-        loader = DirectoryLoader(f'{category_path}', glob="**/*.txt" ,  use_multithreading=True , show_progress=True)
-        data[category_label]= loader
-    return data
-
-data = load_data_from_folders("archive_2")
-
-Culture = data["Culture"].load()
-
-
 
     
 llm = ChatGoogleGenerativeAI(
@@ -44,28 +30,6 @@ llm = ChatGoogleGenerativeAI(
 )
 
 
-
-prompt = """
-You are an Ai bot your role is to answer the users questions form the knowledge  that in retriver 
-at the end of the answer thank the user 
-The answer must be detaild and no less than 100 words.
-
-what to do if the answer is not envluded in the prompt or the context 
-    1. apologies to the user.
-    2. tell the user that you do not know the answer for the asked question 
-    3. ask the user if he has more question to ask. 
-    4. do not mention anything about the context.
-
-for the asnwer:
-    1. The output must be the answer only without any additional thoughts..
-
-knowledge you know:
-{context}
-
-Question : {question}
-
-answer:
-"""
 class embedding:
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
@@ -77,31 +41,70 @@ class embedding:
 
 
 
-
-docs = Culture
-prompt = ChatPromptTemplate.from_template(prompt)
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=embedding())
-
-# Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
-
-
-
-def format_docs(docs):
-    return " ".join(doc.page_content for doc in docs)
+@st.cache_data
+def load_data_from_folders(main_folder_path):
+    data = {}
+    for category_folder in os.listdir(main_folder_path):
+        category_label = category_folder 
+        category_path = os.path.join(main_folder_path, category_folder)
+        loader = DirectoryLoader(f'{category_path}', glob="**/*.txt" ,  use_multithreading=True , show_progress=True)
+        data[category_label]= loader
+        
+    
+    return data["Culture"].load()
 
 
+def response_llm(prmpt):
+    Culture = load_data_from_folders("archive_2")
 
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
-import streamlit as st
+
+    docs = Culture
+
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=embedding())
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+
+
+    def format_docs(docs):
+        return " ".join(doc.page_content for doc in docs)
+
+
+    prompt = """
+    You are an Ai bot your role is to answer the users questions form the knowledge  that in retriver 
+    at the end of the answer thank the user 
+    The answer must be detaild and no less than 100 words.
+
+    what to do if the answer is not envluded in the prompt or the context 
+        1. apologies to the user.
+        2. tell the user that you do not know the answer for the asked question 
+        3. ask the user if he has more question to ask. 
+        4. do not mention anything about the context.
+
+    for the asnwer:
+        1. The output must be the answer only without any additional thoughts..
+
+    knowledge you know:
+    {context}
+
+    Question : {question}
+
+    answer:
+    """
+    prompt = ChatPromptTemplate.from_template(prompt)
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return rag_chain.invoke(prmpt)
 
 
 st.title("Simple chat")
@@ -120,13 +123,13 @@ if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-
+        
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
     
     with st.chat_message("assistant"):
-        response = st.write(rag_chain.invoke(prompt))
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        response = st.write(response_llm(prompt))
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
